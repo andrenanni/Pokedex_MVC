6130428cb1e041fbd2d2ebe59215519a67c16965
andrenanni <andre-nanni@hotmail.com>
2024-02-27 20:39:09 -0300

    Incluindo os tipos no HomeController e apresentaçâo dos tipos na página Index. Criação da página Details.

--- Pokedex/Models/Tipo.cs
+++ Pokedex/Models/Tipo.cs
@@ -3,6 +3,6 @@ namespace Pokedex.Models;
     public class Tipo
     {
         public string Nome { get; set; }
-        public string construtor { get; set; }
+        public string Cor { get; set; }
         
     }
